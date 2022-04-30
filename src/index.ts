998243ede42705d2213e36c6e6d719a8e1bb030e
Kanezoh <asfaltmanta114@gmail.com>
2022-04-30 13:24:00 +0900

    chess program

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath } from "url";
+
 function add(a: number, b: number) {
     return a + b;
 }
@@ -172,3 +174,67 @@ type WarnUser = {
 //    console.log(warning)
 //}
 //warnUser.wasCalled = false
+
+//type Filter = {
+//    <T>(array: T[], f: (item: T) => boolean): T[]
+//}
+//let filter: Filter = (array, f) => {
+//    
+//}
+
+// ex. chess
+
+class Game {
+  private pieces = Game.makePieces()
+
+  private static makePieces() {
+    return [
+      // Kings
+      new King('White', 'E', 1),
+      new King('White', 'E', 8),
+    ]
+  }
+
+}
+
+type Color = 'Black' | 'White'
+type File = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H'
+type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+
+class Position {
+  constructor (
+    private file: File,
+    private rank: Rank
+  ) {}
+
+  distanceFrom(position: Position) {
+    return {
+      rank: Math.abs(position.rank - this.rank),
+      file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0))
+    }
+  }
+}
+
+abstract class Piece {
+  protected position: Position
+  constructor (
+    private readonly color: Color,
+    file: File,
+    rank: Rank
+  ) {
+    this.position = new Position(file, rank)
+  }
+
+  moveTo(position: Position) {
+    this.position = position
+  }
+  abstract canMoveTo(position: Position): boolean
+}
+
+class King extends Piece {
+  canMoveTo(position: Position): boolean {
+    let distance = this.position.distanceFrom(position)
+    return distance.rank < 2 && distance.file < 2
+  }
+}
+
